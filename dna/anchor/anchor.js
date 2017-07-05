25ae7f31e975ccc2eaba5923204281130c7324ff
rasika <rasika.chafekar@gmail.com>
2017-07-05 12:29:52 -0700

    Updated Create Anchor type

--- dna/anchor/anchor.js
+++ dna/anchor/anchor.js
@@ -4,10 +4,17 @@ function genesis() {
 }
 
 //USED IN GENESIS TO AN THE INITIAL ANCHOR
+
+function getMainAchorHash()
+{
+  var anchorMain = {Anchor_Type:"Anchor_Type",Anchor_Text:""};
+  var hashAnchorMain = makeHash(anchorMain);
+  return hashAnchorMain;
+}
 function addAnchor()
 {
   var dna = App.DNA.Hash;
-  var anchor_main = {Anchor_Type:"Anchor_Type",Anchor_Text:""}
+  var anchor_main = {Anchor_Type:"Anchor_Type",Anchor_Text:""};
   var key=commit("anchor",anchor_main);
   commit("directory_links", {Links:[{Base:dna,Link:key,Tag:"Anchor"}]});
   return anchor;
@@ -16,10 +23,13 @@ function addAnchor()
 //USED TO CREATE A NEW Anchor_Type
 function anchor_type_create(anchor_type)
 {
-  var anchor_main = {Anchor_Type:"Anchor_Type",Anchor_Text:""};
-  var anchor_main_hash=makeHash(anchor_main);
-  var _anchor_type_="_anchor_type_"+anchor_type;
-  var new_anchor= {Anchor_Type:_anchor_type_,Anchor_Text:anchor_type};
+  var anchor_main_hash=getMainAchorHash();
+  var new_anchor= {Anchor_Type:anchor_type,Anchor_Text:""};
   var key=commit("anchor",new_anchor);
   commit("anchor_Links",{Links:[{Base:anchor_main_hash,Link:key,Tag:"Anchor_Type"}]});
+}
+
+function anchor_create(anchor_type,anchor_text)
+{
+
 }
