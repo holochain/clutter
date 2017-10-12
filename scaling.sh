66323cdcca4ed37a5fb4384ecc320177bc7a494d
Eric Harris-Braun <eric@harris-braun.com>
2017-10-12 13:05:42 +0200

    scaling test scripting

---
+++ scaling.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+CLONES=2
+REPS=4
+PAUSE=10
+SERVERS=1
+for i in "$@"
+do
+    case $i in
+        -r=*|--reps=*)
+            REPS="${i#*=}"
+            shift # past argument=value
+            ;;
+        -c=*|--clones=*)
+            CLONES="${i#*=}"
+            shift # past argument=value
+            ;;
+        -p=*|--pause=*)
+            PAUSE="${i#*=}"
+            shift # past argument=value
+            ;;
+        --default)
+            DEFAULT=YES
+            shift # past argument with no value
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+done
+
+DURATION=$((PAUSE+2))
+
+#cd clutter
+git checkout test
+cd test/scaling
+echo "DURATION:$DURATION"
+sed -i "s/!DURATION!/$DURATION/" _config.json
+sed -i "s/!CLONES!/$CLONES/" _config.json
+sed -i "s/!CLONES!/$CLONES/" clone.json
+sed -i "s/!SERVERS!/$SERVERS/" clone.json
+sed -i "s/!REPS!/$REPS/" clone.json
+sed -i "s/!PAUSE!/$PAUSE/" clone.json
+echo "starting hcdev for $SERVER_ID"
+#$HOME/go/bin/hcdev -bootstrapServer=bootstrap.holochain.net:10000 -keepalive -serverID=fish.$SERVER_ID scenario -outputDir=../results scaling
