9e72788270c57fffbbf367bb2348cbd4bea935ee
Philip Beadle <philip.beadle@live.com.au>
2017-12-12 17:38:37 +1100

    Update the port in the test to match docker

--- ui-automation/cypress/integration/handle.js
+++ ui-automation/cypress/integration/handle.js
@@ -1,7 +1,7 @@
 describe('This section describes how to manage your Clutter handle', function(){
   it('When you first visit Clutter you will be prompted to create a handle (that\'s the name others will be able to find you by.)', function(){
     // https://on.cypress.io/visit
-    cy.visit('http://localhost:4141')
+    cy.visit('http://localhost:3141')
 
     // Here we've made our first assertion using a '.should()' command.
     // An assertion is comprised of a chainer, subject, and optional value.
