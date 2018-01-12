0a448333ced266c83160960891a4112b086b358e
Philip Beadle <philip.beadle@live.com.au>
2018-01-12 21:07:28 +1100

    New travis steps to use docker-compose

--- ui-automation/cypress/integration/handle.js
+++ ui-automation/cypress/integration/handle.js
@@ -1,6 +1,5 @@
-describe('This section describes how to manage your Clutter handle', function(){
-  it('Let\'s check that all 3 instances of Clutter are running.)', function(){
-    // https://on.cypress.io/visit
+describe('This section describes how to manage your Clutter handle', function () {
+  it('Let\'s check that all 3 instances of Clutter are running.)', function () {
     cy.visit('http://localhost:3141')
     cy.title().should('include', 'Clutter')
     cy.request('http://localhost:4141')
