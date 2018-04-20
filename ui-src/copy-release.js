25b9a143f015ea5dc4e6ed786305b8c7b46c5b59
RhysAnexture <rhys@anexture.com>
2018-04-20 00:23:19 -0400

    Changed the build and release so that the dna folder is copied rather than moved.

--- ui-src/copy-release.js
+++ ui-src/copy-release.js
@@ -1,8 +1,14 @@
-const fs = require('fs')
-const path = require('path')
-var rimraf = require('rimraf')
-rimraf.sync('../release')
-fs.mkdirSync(path.join(__dirname, '../release'))
-fs.renameSync('../ui', '../release/ui')
-fs.renameSync('../dna', '../release/dna')
-fs.createReadStream('../docker-compose.yml').pipe(fs.createWriteStream('../release/docker-compose.yml'))
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const ncp = require('ncp').ncp;
+
+rimraf.sync('../release');
+fs.mkdirSync(path.join(__dirname, '../release'));
+ncp('../ui', '../release/ui', function (err) {
+    if (err) { return console.error(err); }
+});
+ncp('../dna', '../release/dna', function (err) {
+    if (err) { return console.error(err); }
+});
+fs.createReadStream('../docker-compose.yml').pipe(fs.createWriteStream('../release/docker-compose.yml'));
