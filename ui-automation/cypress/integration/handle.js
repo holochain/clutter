9c86c058752edeed2e797fb43b836b105ed2706f
Lindsey <lindseywoo@gmail.com>
2018-05-21 14:03:36 -0700

    changed ui-automation tests to test new functionality

--- ui-automation/cypress/integration/handle.js
+++ ui-automation/cypress/integration/handle.js
@@ -1,16 +1,43 @@
-describe('This section describes how to manage your Clutter handle', function () {
-  it('Let\'s go to the Settings and set the handle others will be able to find us by', function () {
-    cy.visit('/')
-    cy.get('#changeHandleButton').click()
-    cy.get('#myHandle').type('phil')
-    cy.get('#setHandleButton').click()
-    cy.get('#handle').should('contain', 'phil')
-  })
-  it('Now update your handle to something new', function () {
-    cy.visit('/')
-    cy.get('#changeHandleButton').click()
-    cy.get('#myHandle').type('lucy')
-    cy.get('#setHandleButton').click()
-    cy.get('#handle').should('contain', 'lucy')
-  })
-})
+describe('This section describes how to manage your Clutter handle', function() {
+  it("Let's set the handle others will be able to find us by", function() {
+    cy.visit('/', {
+      onBeforeLoad: win => {
+        win.onerror = null;
+      }
+    });
+    cy.get('#myHandle').type('phil');
+    cy.get('#setHandleButton').click();
+    cy.get('#handle').should('contain', 'phil');
+  });
+  it("Let's go to Edit Profile and set the firstName (handle stays the same)", function() {
+    cy.visit('/', {
+      onBeforeLoad: win => {
+        win.onerror = null;
+      }
+    });
+    cy.get('#handle').should('contain', 'phil');
+    cy.get('#changeHandleButton').click();
+    cy
+      .get('form')
+      .get('#inputName')
+      .should('have.value', 'phil');
+    cy
+      .get('form')
+      .get('#inputName')
+      .type('lip');
+    cy.get('#saveChanges').click();
+    cy.get('#handle').should('contain', 'phil');
+    cy.get('#changeHandleButton').click();
+    cy
+      .get('form')
+      .get('#inputName')
+      .should('have.value', 'phillip');
+  });
+  // it('Now update your handle to something new', function() {
+  //   cy.visit('/');
+  //   cy.get('#changeHandleButton').click();
+  //   cy.get('#myHandle').type('lucy');
+  //   cy.get('#setHandleButton').click();
+  //   cy.get('#handle').should('contain', 'lucy');
+  // });
+});
--- ui-src/src/EditProfile.js
+++ ui-src/src/EditProfile.js
@@ -1,43 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class EditProfile extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       newNameText: ''
-    }
+    };
   }
   componentWillMount() {
-    const { firstName, getFirstName } = this.props
-    getFirstName()
-    this.setState({ newNameText: firstName })
+    const { firstName, getFirstName } = this.props;
+    getFirstName();
+    this.setState({ newNameText: firstName });
   }
   componentDidUpdate(prevProps) {
-    const { firstName } = this.props
+    const { firstName } = this.props;
     if (!prevProps.firstName && firstName) {
-      this.setState({ newNameText: firstName })
+      this.setState({ newNameText: firstName });
     }
   }
   updateNameText = e => {
     this.setState({
       newNameText: e.target.value
-    })
-  }
+    });
+  };
   onHandleSubmit = e => {
-    const { newNameText } = this.state
-    const { history, firstName, setFirstName } = this.props
-    e.preventDefault()
+    const { newNameText } = this.state;
+    const { history, firstName, setFirstName } = this.props;
+    e.preventDefault();
 
-    if (!newNameText) return
-    if (!(newNameText === firstName)) setFirstName(newNameText)
-    else this.setState({ newNameText: firstName })
+    if (!newNameText) return;
+    if (!(newNameText === firstName)) setFirstName(newNameText);
+    else this.setState({ newNameText: firstName });
     // Redirect user to main page
-    history.push('/')
-  }
+    history.push('/');
+  };
 
   render() {
-    const { handle } = this.props
-    const { newNameText } = this.state
+    const { handle } = this.props;
+    const { newNameText } = this.state;
     return (
       <div className="panel panel-default">
         <div className="panel-body">
@@ -49,7 +49,7 @@ class EditProfile extends Component {
             <div className="form-row">
               <div className="form-group col-xs-6">
                 <label>handle</label>
-                <p>@{handle}</p>
+                <p id="handle">@{handle}</p>
               </div>
               <div className="form-group col-xs-6">
                 <label>name</label>
@@ -86,8 +86,8 @@ class EditProfile extends Component {
           </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default EditProfile
+export default EditProfile;
