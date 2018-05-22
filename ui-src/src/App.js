1a55e1a89a4dd6f2b6b995083ab97dfa8113ff28
Micah J <micalangelone@gmail.com>
2018-05-22 18:26:03 -0500

    Integrating UI updates

--- ui-src/src/App.js
+++ ui-src/src/App.js
@@ -43,39 +43,53 @@ class App extends Component {
         <div className='error transition500' />
         <div className='row first'>
           <div className='fixed-area'>
-          <div className='col-sm-2 contentcontainer'>
-            <div className='logo'>
-              <img src='/cat-eating-bird-circle.png' alt='cat eating bird' />
-              <Link to='/' id='handle'>{this.props.handle}</Link>
-            </div>
-          </div>
-          <div className="col-sm-7">
-            <div className="contentcontainer">
-              <div>
-
-          <div className='col-sm-3'>
-            <div className='alphabox'>
-              <div id='about'>
-                <h2>What is Clutter?</h2>
-                <p>
-                  A{' '}
-                  <a href="https://en.wiktionary.org/wiki/clutter">
-                    <em>clutter</em>
-                  </a>{' '}
-                  is a flock of cats.
-                </p>
-                <p>
-                  <strong>Clutter</strong> is a fully decentralized alternative
-                  to Twitter.
-                </p>
-                <p>Impossible to censor or control.</p>
-                <p>
-                  Join the mewvolution on{' '}
-                  <a href="http://holochain.org">holochain.org</a>.
-                </p>
+            <div className='col-sm-2 contentcontainer'>
+              <div className='logo'>
+                <img src='/cat-eating-bird-circle.png' alt='cat eating bird' />
+                <Link to='/' id='handle'>{handle}</Link>
               </div>
             </div>
-          </div>
+            <div className="col-sm-7">
+              <div className='contentcontainer'>
+                <div>
+                  <Link to='/editProfile' id='changeHandleButton' className='btn btn-default'>Profile</Link>
+                </div>
+                <Link to='/follow' id='followButton' className='btn btn-default'>Follow People</Link>
+                <div id='banner'>
+                  <a href="/">Clutter</a>
+                  <div className='subtitle'>can haz herd cats?</div>
+                </div>
+                <div id='content'>
+                  <Route path='/' exact component={NewMeowContainer} />
+                  <Route path='/editProfile' component={EditProfileContainer} />
+                  <Route path='/follow' component={FollowContainer} />
+                  <Route path='/meow/:meowHash' component={MeowContainer} />
+                </div>
+              </div>
+            </div>
+
+            <div className='col-sm-3'>
+              <div className='alphabox'>
+                <div id='about'>
+                  <h2>What is Clutter?</h2>
+                  <p>
+                    <a href="https://en.wiktionary.org/wiki/clutter">
+                      <em>clutter</em>
+                    </a>{' '}
+                    is a flock of cats.
+                  </p>
+                  <p>
+                    <strong>Clutter</strong> is a fully decentralized alternative
+                    to Twitter.
+                  </p>
+                  <p>Impossible to censor or control.</p>
+                  <p>
+                    Join the mewvolution on{' '}
+                    <a href="http://holochain.org">holochain.org</a>.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
           <div className='row'>
             <div className='contentcontainer' id='feedContent'>
@@ -83,7 +97,7 @@ class App extends Component {
                 <Route path='/' exact component={FollowingFeedContainer} />
                 <Route path='/u/:handle' component={UserFeedContainer} />
               </div>
-              </div>
+            </div>
           </div>
         </div>
       </div>
