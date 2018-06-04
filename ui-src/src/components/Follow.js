aaae3b57852e4ace2accfc1cd3466005ea4ac9f4
Lindsey <lindseywoo@gmail.com>
2018-06-04 08:06:36 -0700

    added conditional to show follow and unfollow panels there are items

--- ui-src/src/components/Follow.js
+++ ui-src/src/components/Follow.js
@@ -27,109 +27,119 @@ class Follow extends Component {
         <div className="panel-body">
           <div className="row">
             <h3>Following</h3>
-            <div
-              class="panel-body"
-              style={{ 'overflow-y': 'scroll', height: '100px' }}
-            >
-              <div
-                class="mid-width wrapItems"
-                style={{
-                  'padding-top': '10px',
-                  'background-color': 'pink',
-                  height: '100px'
-                }}
-              >
-                <ul id="following">
-                  {this.props.following.length === 0 && (
-                    <li>You currently aren't following anyone.</li>
-                  )}
-                  {this.props.following.map(user => {
-                    return (
-                      <li className="following-handle" key={user.handle}>
-                        <div className="col-xs-9">
-                          <span className="handle">{user.handle}</span>
-                        </div>
-                        <div
-                          className="col-xs-3"
-                          style={{ 'padding-bottom': '10px' }}
-                        >
-                          <button
-                            type="button"
-                            className="btn btn-default"
-                            onClick={() => this.props.unfollow(user.handle)}
+
+            <ul id="following">
+              {this.props.following.length === 0 && (
+                <li>You currently aren't following anyone.</li>
+              )}
+              {this.props.following.length > 0 && (
+                <div
+                  class="panel-body"
+                  style={{ 'overflow-y': 'scroll', height: '100px' }}
+                >
+                  <div
+                    class="mid-width wrapItems"
+                    style={{
+                      'padding-top': '10px',
+                      'background-color': '#eeeeee',
+                      height: '100px'
+                    }}
+                  >
+                    {this.props.following.map(user => {
+                      return (
+                        <li className="following-handle" key={user.handle}>
+                          <div className="col-xs-9">
+                            <span className="handle">{user.handle}</span>
+                          </div>
+                          <div
+                            className="col-xs-3"
+                            style={{ 'padding-bottom': '10px' }}
                           >
-                            Unfollow
-                          </button>
-                        </div>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            </div>
+                            <button
+                              type="button"
+                              className="btn btn-default"
+                              onClick={() => this.props.unfollow(user.handle)}
+                            >
+                              Unfollow
+                            </button>
+                          </div>
+                        </li>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+            </ul>
           </div>
           <div className="row">
-            <div
-              class="panel-body"
-              style={{ 'overflow-y': 'scroll', height: '300px' }}
-            >
-              <div class="mid-width wrapItems" style={{ height: '300px' }}>
-                <h3 id="myModalLabel">Follow someone</h3>
-                <div className="col-xs-12">
-                  <div className="form-group input-icon">
-                    <i>@</i>
-                    <input
-                      value={this.state.newFollowText}
-                      onChange={this.updateFollowText}
-                      type="text"
-                      className="form-control"
-                      id="followHandle"
-                      placeholder="handle"
-                    />
-                  </div>
-                </div>
-                <ul id="not-following">
-                  {filteredNotFollowing.length === 0 && (
-                    <li>
-                      There are no users that you aren't already following.
-                    </li>
-                  )}
-                  {filteredNotFollowing.map(user => {
-                    return (
-                      <li className="following-handle" key={user.handle}>
-                        <div className="col-xs-9">
-                          <span className="handle">{user.handle}</span>
-                        </div>
-                        <div
-                          className="col-xs-3"
-                          style={{ 'padding-bottom': '10px' }}
-                        >
-                          <button
-                            type="button"
-                            className="btn btn-default"
-                            onClick={() => this.props.follow(user.handle)}
+            <h3 id="myModalLabel">Follow someone</h3>
+            <div className="col-xs-12">
+              <div className="form-group input-icon">
+                <i>@</i>
+                <input
+                  value={this.state.newFollowText}
+                  onChange={this.updateFollowText}
+                  type="text"
+                  className="form-control"
+                  id="followHandle"
+                  placeholder="handle"
+                />
+              </div>
+            </div>
+            <ul id="not-following">
+              {filteredNotFollowing.length === 0 && (
+                <li>There are no users that you aren't already following.</li>
+              )}
+              {filteredNotFollowing.length > 0 && (
+                <div
+                  class="panel-body"
+                  style={{ 'overflow-y': 'scroll', height: '200px' }}
+                >
+                  <div
+                    class="mid-width wrapItems"
+                    style={{
+                      'padding-top': '10px',
+                      'background-color': '#eeeeee',
+                      height: '200px'
+                    }}
+                  >
+                    {filteredNotFollowing.map(user => {
+                      return (
+                        <li className="following-handle" key={user.handle}>
+                          <div className="col-xs-9">
+                            <span className="handle">{user.handle}</span>
+                          </div>
+                          <div
+                            className="col-xs-3"
+                            style={{ 'padding-bottom': '10px' }}
                           >
-                            Follow
-                          </button>
-                        </div>
-                      </li>
-                    )
-                  })}
-                </ul>
-                <div class="row">
-                  <div class="col-sm-1" />
-                  <div class="col-sm-4" />
-                  <div class="col-sm-6">
-                    <button
-                      type="button"
-                      id="close"
-                      className="btn btn-primary pull-right"
-                      onClick={() => this.props.history.push('/')}
-                    >
-                      Close
-                    </button>
+                            <button
+                              type="button"
+                              className="btn btn-default"
+                              onClick={() => this.props.follow(user.handle)}
+                            >
+                              Follow
+                            </button>
+                          </div>
+                        </li>
+                      )
+                    })}
                   </div>
                 </div>
+              )}
+            </ul>
+            <div class="row">
+              <div class="col-sm-1" />
+              <div class="col-sm-4" />
+              <div class="col-sm-6">
+                <button
+                  type="button"
+                  id="close"
+                  className="btn btn-primary pull-right"
+                  onClick={() => this.props.history.push('/')}
+                >
+                  Close
+                </button>
               </div>
             </div>
           </div>
